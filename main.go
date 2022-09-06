68c2c19725317646f0cdcaa55e7b3ed72880e51c
petrus.prinsloo@pargo.co.za <petrus.prinsloo@pargo.co.za>
2022-09-06 20:29:41 +0200

    Skip lines with comments

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 func check(e error) {
@@ -23,10 +25,29 @@ func main() {
 	file, err := os.Open(filename)
 
 	check(err)
-	defer file.Close()
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Error closing file")
+			log.Fatal(err)
+		}
+	}(file)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
+		line := strings.Trim(scanner.Text(), " \t")
+		if len(line) == 0 || line[0] == '#' {
+			continue
+		}
+
 		fmt.Println(scanner.Text())
 	}
+}
+
+func convert16to8bytes(s string) []string {
+	var r []string
+	for i := 0; i < len(s); i += 2 {
+		r = append(r, string(s[i]))
+	}
+	return r
 }
