7bc9d48cc804a318f82d8540cdcb9203d103b022
petrus.prinsloo@pargo.co.za <petrus.prinsloo@pargo.co.za>
2022-09-06 21:16:43 +0200

    Add simple case to see that strings clean and split properly

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -20,6 +21,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	//create new file
+	asmFile, err := os.OpenFile("output.h", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+
 	filename := os.Args[1]
 
 	file, err := os.Open(filename)
@@ -34,6 +38,7 @@ func main() {
 	}(file)
 
 	scanner := bufio.NewScanner(file)
+	var assembled string
 	for scanner.Scan() {
 		line := strings.Trim(scanner.Text(), " \t")
 		if len(line) == 0 || line[0] == '#' {
@@ -41,6 +46,8 @@ func main() {
 		}
 
 		fmt.Println(scanner.Text())
+		assembled = assembleLine(cleanString(line))
+		writeToFile(asmFile, assembled)
 	}
 }
 
@@ -50,4 +57,39 @@ func convert16to8bytes(s string) []string {
 		r = append(r, string(s[i]))
 	}
 	return r
+}
+
+func writeToFile(file *os.File, s string) {
+	_, err := file.WriteString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func cleanString(s string) string {
+	re := regexp.MustCompile(`\s+`)
+	return re.ReplaceAllString(s, " ")
+}
+
+func assembleLine(line string) string {
+	var assembled string
+	//split line into parts
+	parts := strings.Split(line, " ")
+	//get the first part of the line
+	opcode := strings.ToLower(parts[1])
+	//get the rest of the parts
+	//args := parts[1:]
+	//switch on the opcode
+	switch opcode {
+	case "nop":
+		assembled = "00"
+		break
+	case "LXI":
+		assembled = "01"
+
+	default:
+		fmt.Println("Invalid opcode")
+		//os.Exit(1)
+	}
+	return assembled
 }
