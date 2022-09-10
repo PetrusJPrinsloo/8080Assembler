7e65ba95acd9357d0739664472b4102113a070a1
petrus.prinsloo@pargo.co.za <petrus.prinsloo@pargo.co.za>
2022-09-10 13:38:07 +0200

    INR, DCR, MVI, RLC, LDAX, DCX, done

--- main.go
+++ main.go
@@ -147,13 +147,136 @@ func assembleLine(line string) string {
 		}
 		break
 	case "INR":
-		assembled = "04"
-		//todo: check if B, C, D, E, H, L, M, A
+		switch parts[2] {
+		case "B":
+			assembled = "04"
+			break
+		case "C":
+			assembled = "0C"
+			break
+		case "D":
+			assembled = "14"
+			break
+		case "E":
+			assembled = "1C"
+			break
+		case "H":
+			assembled = "24"
+			break
+		case "L":
+			assembled = "2C"
+			break
+		case "M":
+			assembled = "34"
+			break
+		case "A":
+			assembled = "3C"
+			break
+		}
 		break
 	case "DCR":
-		assembled = "05"
-		//todo: check if B, C, D, E, H, L, M, A
+		switch parts[2] {
+		case "B":
+			assembled = "05"
+			break
+		case "C":
+			assembled = "0D"
+			break
+		case "D":
+			assembled = "15"
+			break
+		case "E":
+			assembled = "1D"
+			break
+		case "H":
+			assembled = "25"
+			break
+		case "L":
+			assembled = "2D"
+			break
+		case "M":
+			assembled = "35"
+			break
+		case "A":
+			assembled = "3D"
+			break
+		}
 		break
+	case "MVI":
+		bytes = convertToBytes(args)
+		switch parts[2] {
+		case "B":
+			assembled = "06" + bytes[1]
+			break
+		case "C":
+			assembled = "0E" + bytes[1]
+			break
+		case "D":
+			assembled = "16" + bytes[1]
+			break
+		case "E":
+			assembled = "1E" + bytes[1]
+			break
+		case "H":
+			assembled = "26" + bytes[1]
+			break
+		case "L":
+			assembled = "2E" + bytes[1]
+			break
+		case "M":
+			assembled = "36" + bytes[1]
+			break
+		case "A":
+			assembled = "3E" + bytes[1]
+			break
+		}
+		break
+	case "RLC":
+		assembled = "07"
+		break
+	case "DAD":
+		switch parts[2] {
+		case "B":
+			assembled = "09"
+			break
+		case "D":
+			assembled = "19"
+			break
+		case "H":
+			assembled = "29"
+			break
+		case "SP":
+			assembled = "39"
+			break
+		}
+		break
+	case "LDAX":
+		switch parts[2] {
+		case "B":
+			assembled = "0A"
+			break
+		case "D":
+			assembled = "1A"
+			break
+		}
+		break
+	case "DCX":
+		switch parts[2] {
+		case "B":
+			assembled = "0B"
+			break
+		case "D":
+			assembled = "1B"
+			break
+		case "H":
+			assembled = "2B"
+			break
+		case "SP":
+			assembled = "3B"
+			break
+		}
+		break
+
 	default:
 		fmt.Println("Invalid opcode")
 		//os.Exit(1)
